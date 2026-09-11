5b3a1d38a8180f816ec52b08c1b6098ea819cfac
wellington <wellington281482@gmail.com>
2026-09-11 20:19:24 -0300

    aquivos deletados

--- app/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-SisPatrimônio - Sistema de Gestão de Patrimônio e Rastreamento de Fluxo
-"""
-
-__version__ = "1.0.0"
--- app/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.api.v1_router import api_v1_router
-
-__all__ = ["api_v1_router"]
