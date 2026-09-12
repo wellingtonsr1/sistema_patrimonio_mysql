2fb57924427a2402a555c06ea10defe11b941a35
wellington <wellington281482@gmail.com>
2026-09-12 17:58:00 -0300

    Alteração do IP do server em config.py

--- app/config.py
+++ app/config.py
@@ -36,7 +36,7 @@ if not DATABASE_URL:
 APP_NAME = "SisPatrimônio Pro"
 APP_DESCRIPTION = "Sistema Integrado de Gestão Patrimonial e Fluxo de Movimentação de Equipamentos"
 APP_VERSION = "1.0.0"
-APP_HOST = os.getenv("APP_HOST", "127.0.0.1")
+APP_HOST = os.getenv("APP_HOST", "192.168.0.9")
 APP_PORT = int(os.getenv("APP_PORT", "8000"))
 
 # Organização padrão para emissão de termos
