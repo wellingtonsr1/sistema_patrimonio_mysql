d4c05ee2d40fd2bdf6696a042f0dad4ec6a14d26
wellingtonsr1 <wellington281482@gmail.com>
2026-09-25 11:24:48 -0300

    Remove wrapper temporario do deploy da 036

--- _deploy036.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Wrapper temporario para passar mensagem com espacos ao deploy.bat
-call "D:\IA\sistema_patrimonio_mysql\deploy.bat" "Feature 036 - larguras proporcionais nas colunas da tabela de conferencia"
